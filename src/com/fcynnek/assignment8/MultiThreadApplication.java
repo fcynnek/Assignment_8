a9fb0934360bba301c4b34b3b8c88744741b2d6c
fcynnek <kchengfung@gmail.com>
2023-02-15 19:29:48 -0500

    Multi Thread Reading of File

---
+++ src/com/fcynnek/assignment8/MultiThreadApplication.java
@@ -0,0 +1,38 @@
+package com.fcynnek.assignment8;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
+
+import com.coderscampus.assignment.Assignment8;
+
+public class MultiThreadApplication {
+
+	public static void main(String[] args) {
+		
+		Assignment8 baseCode = new Assignment8();
+		ExecutorService cachedPool = Executors.newCachedThreadPool();
+		ExecutorService cpuPool = Executors.newFixedThreadPool(5);
+		List<Integer> listOfNumbers = new ArrayList<Integer>();
+		
+		for (int i=0; i<1000; i++) {
+			listOfNumbers.addAll(baseCode.getNumbers());
+//			System.out.println(listOfNumbers);
+			
+			
+//			CompletableFuture.supplyAsync(() -> new Assignment8(), cachedPool)
+//							 .thenApplyAsync(baseCode.getNumbers(), cpuPool)
+//							 .thenAcceptAsync((Consumer<? super Assignment8>) baseCode.getNumbers(), cpuPool);
+		}
+		
+	}
+}
