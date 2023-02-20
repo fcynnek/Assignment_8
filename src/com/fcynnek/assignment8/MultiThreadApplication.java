6c844362759180803637c077fc95f3535fe9a125
fcynnek <kchengfung@gmail.com>
2023-02-20 18:46:29 -0500

    Working Async getNumbers()

--- src/com/fcynnek/assignment8/MultiThreadApplication.java
+++ src/com/fcynnek/assignment8/MultiThreadApplication.java
@@ -2,7 +2,9 @@ package com.fcynnek.assignment8;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -20,18 +22,20 @@ public class MultiThreadApplication {
 	public static void main(String[] args) {
 		
 		Assignment8 baseCode = new Assignment8();
-//		ExecutorService cachedPool = Executors.newCachedThreadPool();
-		ExecutorService cpuPool = Executors.newFixedThreadPool(5);
+		ExecutorService cachedPool = Executors.newCachedThreadPool();
+		ExecutorService cpuPool = Executors.newFixedThreadPool(6);
 		List<Integer> listOfNumbers = new ArrayList<Integer>();
+		Map<Integer, Integer> countOfNumbers = new HashMap<>();
 		
 		for (int i=0; i<1000; i++) {
 //			listOfNumbers.addAll(baseCode.getNumbers());
 //			System.out.println(listOfNumbers);
 			
 			
-			CompletableFuture.supplyAsync(() -> new Assignment8())
-							 .thenAccept(numbers -> listOfNumbers.addAll(baseCode.getNumbers()))
-							 .completeAsync();
+//			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cachedPool);
+			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cpuPool);
+//							 .thenComposeAsync(listOfNumbers.addAll(baseCode.getNumbers()));
+//							 .thenAccept(numbers -> baseCode.getNumbers())
 //							 .thenApplyAsync(number -> baseCode.getNumbers(), cpuPool)
 //							 .thenAcceptAsync(number -> baseCode.getNumbers());
 //							 .thenAcceptAsync(iterances -> System.out.println());
@@ -39,7 +43,20 @@ public class MultiThreadApplication {
 //							 .thenAcceptAsync((Consumer<? super Assignment8>) baseCode.getNumbers(), cpuPool);
 		}
 		
+//		for (Integer number : listOfNumbers) {
+//			countOfNumbers.compute(number, (k, v) -> v == null ? 1 : v + 1);
+//		}
+		for (int i=0; i<1000000; i++) {
+			listOfNumbers.forEach(number -> {
+				if (countOfNumbers.containsKey(number)) {
+					countOfNumbers.put(number, countOfNumbers.get(countOfNumbers) + 1);
+				} else {
+					countOfNumbers.put(number, 1);
+				}
+			});
+		}
+
+		countOfNumbers.forEach((k, v) -> System.out.println(k + ": " + v));
+		
 	}
-	
-	//then run instead of then applyasync
 }
