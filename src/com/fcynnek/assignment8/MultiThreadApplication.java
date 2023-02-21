0ca5ca7040ee4c2d0cf75263670ed749d20d7ac5
fcynnek <kchengfung@gmail.com>
2023-02-21 16:38:01 -0500

    Trying Streams

--- src/com/fcynnek/assignment8/MultiThreadApplication.java
+++ src/com/fcynnek/assignment8/MultiThreadApplication.java
@@ -32,8 +32,10 @@ public class MultiThreadApplication {
 //			System.out.println(listOfNumbers);
 			
 			
-//			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cachedPool);
-			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cpuPool);
+			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cachedPool);
+//			CompletableFuture.supplyAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cpuPool);
+//			CompletableFuture.supplyAsync(() -> ((List<Integer>) countOfNumbers).addAll(baseCode.getNumbers()), cachedPool);
+//			CompletableFuture.supplyAsync(() -> ((List<Integer>) countOfNumbers).addAll(baseCode.getNumbers()), cpuPool);
 //							 .thenComposeAsync(listOfNumbers.addAll(baseCode.getNumbers()));
 //							 .thenAccept(numbers -> baseCode.getNumbers())
 //							 .thenApplyAsync(number -> baseCode.getNumbers(), cpuPool)
@@ -46,17 +48,24 @@ public class MultiThreadApplication {
 //		for (Integer number : listOfNumbers) {
 //			countOfNumbers.compute(number, (k, v) -> v == null ? 1 : v + 1);
 //		}
-		for (int i=0; i<1000000; i++) {
-			listOfNumbers.forEach(number -> {
-				if (countOfNumbers.containsKey(number)) {
-					countOfNumbers.put(number, countOfNumbers.get(countOfNumbers) + 1);
-				} else {
-					countOfNumbers.put(number, 1);
-				}
-			});
-		}
-
-		countOfNumbers.forEach((k, v) -> System.out.println(k + ": " + v));
+//		for (int i=0; i<=1000000; i++) {
+//			listOfNumbers.forEach(number -> {
+//				if (countOfNumbers.containsKey(number)) {
+//					countOfNumbers.put(number, countOfNumbers.get(countOfNumbers) + 1);
+//				} else {
+//					countOfNumbers.put(number, 1);
+//				}
+//			});
+//		}
+//
+//		countOfNumbers.forEach((k, v) -> System.out.println(k + ": " + v));
 		
+		listOfNumbers.stream()
+					 .forEach(number -> {
+						 Integer key = listOfNumbers.get(number);
+						 Integer value = listOfNumbers.get(key) + 1;
+						 countOfNumbers.put(key, value);
+					 });
+		countOfNumbers.forEach((key, value) -> System.out.println(key + ": " + value));
 	}
 }
