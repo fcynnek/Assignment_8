71f7325c800d9325d6bba2f21185be45603e6fb3
fcynnek <kchengfung@gmail.com>
2023-02-28 20:46:30 -0500

    Not counting correctly

--- src/com/fcynnek/assignment8/MultiThreadApplication.java
+++ src/com/fcynnek/assignment8/MultiThreadApplication.java
@@ -27,23 +27,23 @@ public class MultiThreadApplication {
 		Assignment8 baseCode = new Assignment8();
 		ExecutorService cachedPool = Executors.newCachedThreadPool();
 //		ExecutorService cpuPool = Executors.newFixedThreadPool(6);
-//		List<Integer> listOfNumbers = new ArrayList<>();
-		List<AtomicInteger> listOfNumbers = new ArrayList<>();
-		Map<AtomicInteger, AtomicInteger> countOfNumbers = new HashMap<>();
+		List<Integer> listOfNumbers = new ArrayList<>();
+//		List<AtomicInteger> listOfNumbers = new ArrayList<>();
+		Map<Integer, AtomicInteger> countOfNumbers = new HashMap<>();
 		List<CompletableFuture<Void>> listOfFutures = new ArrayList<>();
 		
 		
 		for (int i=0; i<1000; i++) {
 			
-//			CompletableFuture<Void> future = CompletableFuture.runAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cachedPool);
+			CompletableFuture<Void> future = CompletableFuture.runAsync(() -> listOfNumbers.addAll(baseCode.getNumbers()), cachedPool);
 			
-			CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-				List<Integer> numbers = baseCode.getNumbers();
-				for (int number : numbers) {
-					AtomicInteger atomicNum = new AtomicInteger(number);
-					listOfNumbers.add(atomicNum);
-				}
-			}, cachedPool);
+//			CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+//				List<Integer> numbers = baseCode.getNumbers();
+//				for (int number : numbers) {
+//					AtomicInteger atomicNum = new AtomicInteger(number);
+//					listOfNumbers.addAll(atomicNum);
+//				}
+//			}, cachedPool);
 			
 			listOfFutures.add(future);
 			
@@ -54,35 +54,34 @@ public class MultiThreadApplication {
 		}
 		CompletableFuture.allOf(listOfFutures.toArray(new CompletableFuture[0])).join();
 		
-//		while (listOfFutures.stream().filter(CompletableFuture :: isDone).count() < 1000) {
+		while (listOfFutures.stream().filter(CompletableFuture :: isDone).count() < 1000) {
 //			System.out.println(listOfFutures.stream().filter(CompletableFuture :: isDone).count());
-//		}
+		}
 		
-		listOfNumbers.forEach(number -> {
-			
-				
+//		listOfNumbers.forEach(number -> {
+					
 //				if (countOfNumbers.containsKey(number)) {
 //					countOfNumbers.put(number, countOfNumbers.get(number) + 1);
 //				} else {
 //					countOfNumbers.put(number, 1);
 //				}
 //			}
+		for (Integer number : listOfNumbers) {
+			
 			synchronized (countOfNumbers) {
 				countOfNumbers.compute(number, (k, v) -> {
 					if (v == null) {
-						return new AtomicInteger(1);
+						v = new AtomicInteger();
 					} else {
-						v.incrementAndGet();
-						return v;
+						synchronized (v) {
+							v.incrementAndGet();
+						}
 					}
+					return v;
 				});
-			}});
+			}
+		};
 				
-
-
-					
-
-			
 		
 		countOfNumbers.forEach((k, v) -> System.out.println(k + ": " + v.get()));
 
